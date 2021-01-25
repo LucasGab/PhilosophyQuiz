ae8803e9085bcc4b147e03f86c800da49abe550b
Lucas Silva <lucas.gabriel@mvisia.com.br>
2021-01-25 18:44:07 -0300

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,43 @@
 import styled from 'styled-components'
+import QuizBackground from '../src/components/QuizBackground'
+import Card from '../src/components/Card'
+import Footer from '../src/components/Footer'
+import QuizLogo from '../src/components/QuizLogo'
+import GitHubCorner from '../src/components/GitHubCorner'
+import db from '../db.json';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const QuizContainer = styled.div`
+  width:100%;
+  max-width: 350px;
+  margin: auto 10%;
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return(
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Card>
+          <Card.Header>
+            <h1>{db.title}</h1>
+          </Card.Header>
+          <Card.Content>
+            <p>{db.description}</p>
+            <Card.Input placeholder="Coloque seu nome aqui"></Card.Input>
+            <Card.Button disabled>Jogar</Card.Button>
+          </Card.Content>
+        </Card>
+
+        <Card>
+          <Card.Content>
+            <h2>Quizes da Galera</h2>
+
+            <p>lorem ipsum dolor sit amet...</p>
+          </Card.Content>
+        </Card>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+    </QuizBackground>
+  ) 
 }
---
+++ src/components/Card/index.js
@@ -0,0 +1,88 @@
+import styled from 'styled-components'
+
+const Card = styled.div`
+    margin-top: 24px;
+    margin-bottom: 24px;
+    border: 1px solid ${({ theme }) => theme.colors.primary};
+    background-color: ${({ theme }) => {
+        return theme.colors.primaryBg;
+    }};
+    border-radius: 4px;
+    overflow:hidden;
+
+    h1 , h2, h3{
+        font-size: 16px;
+        font-weight: 700;
+        line-height: 1;
+        color: ${({ theme }) => theme.colors.contrastTextPrimary};
+        margin-bottom: 0;
+    }
+
+    p {
+        font-size: 14px;
+        font-weight: 400;
+        line-height: 1.2;
+        color: ${({ theme }) => theme.colors.contrastTextPrimary};
+    }
+`;
+
+Card.Header = styled.header`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 18px 32px;
+    background-color: ${({ theme }) => theme.colors.primary};
+    * {
+        margin: 0;
+    }
+`;
+
+Card.Content = styled.div`
+    padding: 24px 32px 32px 32px;
+    & > *:first-child {
+        margin-top: 0;
+    }
+    & > *:last-child {
+        margin-bottom: 0;
+    }
+    ul {
+        list-style: none;
+        padding: 0;
+    }
+`;  
+
+Card.Input = styled.input`
+    width:100%;
+    border-radius: 4px;
+    padding: 10px;
+    margin: 10px 0 20px 0;
+
+    outline: none;
+    background-color: ${({ theme }) => theme.colors.primaryBg};
+    border: 2px solid ${({ theme }) => theme.colors.primary};
+    color: ${({ theme }) => theme.colors.contrastTextPrimary};
+    
+    &:focus{
+        background-color: ${({ theme }) => theme.colors.secondaryBg};
+    }
+
+    &::placeholder {
+        color: ${({ theme }) => theme.colors.contrastTextPrimary};
+    }
+`;
+
+Card.Button = styled.button`
+    width:100%;
+    font-weight: bold;
+    letter-spacing: 1px;
+    padding: 10px;
+    border: none;
+    border-radius: 5px;
+    color: ${({ theme }) => theme.colors.contrastTextPrimary};
+
+    &:disabled{
+        background-color: ${({ theme }) => theme.colors.disabled};
+    }
+`
+
+export default Card;
